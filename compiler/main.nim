b3e21922234c9cf442452d567f2a1bb93cf6cc74
stisa <stisa4@gmail.com>
2017-09-27 20:52:24 +0200

    some misc files...

---
+++ compiler/main.nim
@@ -0,0 +1,304 @@
+#
+#
+#           The Nim Compiler
+#        (c) Copyright 2015 Andreas Rumpf
+#
+#    See the file "copying.txt", included in this
+#    distribution, for details about the copyright.
+#
+
+# implements the command dispatcher and several commands
+
+import
+  llstream, strutils, ast, astalgo, lexer, syntaxes, renderer, options, msgs,
+  os, condsyms, rodread, rodwrite, times,
+  wordrecg, sem, semdata, idents, passes, docgen, extccomp,
+  cgen, jsgen, json, nversion,
+  platform, nimconf, importer, passaux, depends, vm, vmdef, types, idgen,
+  docgen2, service, parser, modules, ccgutils, sigmatch, ropes,
+  modulegraphs
+
+from magicsys import systemModule, resetSysTypes
+
+proc rodPass =
+  if optSymbolFiles in gGlobalOptions:
+    registerPass(rodwritePass)
+
+proc codegenPass =
+  registerPass cgenPass
+
+proc semanticPasses =
+  registerPass verbosePass
+  registerPass semPass
+
+proc writeDepsFile(g: ModuleGraph; project: string) =
+  let f = open(changeFileExt(project, "deps"), fmWrite)
+  for m in g.modules:
+    if m != nil:
+      f.writeLine(toFullPath(m.position.int32))
+  f.close()
+
+proc commandGenDepend(graph: ModuleGraph; cache: IdentCache) =
+  semanticPasses()
+  registerPass(gendependPass)
+  #registerPass(cleanupPass)
+  compileProject(graph, cache)
+  writeDepsFile(graph, gProjectFull)
+  generateDot(gProjectFull)
+  execExternalProgram("dot -Tpng -o" & changeFileExt(gProjectFull, "png") &
+      ' ' & changeFileExt(gProjectFull, "dot"))
+
+proc commandCheck(graph: ModuleGraph; cache: IdentCache) =
+  msgs.gErrorMax = high(int)  # do not stop after first error
+  defineSymbol("nimcheck")
+  semanticPasses()            # use an empty backend for semantic checking only
+  rodPass()
+  compileProject(graph, cache)
+
+proc commandDoc2(graph: ModuleGraph; cache: IdentCache; json: bool) =
+  msgs.gErrorMax = high(int)  # do not stop after first error
+  semanticPasses()
+  if json: registerPass(docgen2JsonPass)
+  else: registerPass(docgen2Pass)
+  #registerPass(cleanupPass())
+  compileProject(graph, cache)
+  finishDoc2Pass(gProjectName)
+
+proc commandCompileToC(graph: ModuleGraph; cache: IdentCache) =
+  extccomp.initVars()
+  semanticPasses()
+  registerPass(cgenPass)
+  rodPass()
+  #registerPass(cleanupPass())
+
+  compileProject(graph, cache)
+  cgenWriteModules(graph.backend, graph.config)
+  if gCmd != cmdRun:
+    let proj = changeFileExt(gProjectFull, "")
+    extccomp.callCCompiler(proj)
+    extccomp.writeJsonBuildInstructions(proj)
+
+proc commandJsonScript(graph: ModuleGraph; cache: IdentCache) =
+  let proj = changeFileExt(gProjectFull, "")
+  extccomp.runJsonBuildInstructions(proj)
+
+proc commandCompileToJS(graph: ModuleGraph; cache: IdentCache) =
+  #incl(gGlobalOptions, optSafeCode)
+  setTarget(osJS, cpuJS)
+  #initDefines()
+  defineSymbol("nimrod") # 'nimrod' is always defined
+  defineSymbol("ecmascript") # For backward compatibility
+  defineSymbol("js")
+  if gCmd == cmdCompileToPHP: defineSymbol("nimphp")
+  semanticPasses()
+  registerPass(JSgenPass)
+  compileProject(graph, cache)
+
+proc interactivePasses(graph: ModuleGraph; cache: IdentCache) =
+  #incl(gGlobalOptions, optSafeCode)
+  #setTarget(osNimrodVM, cpuNimrodVM)
+  initDefines()
+  defineSymbol("nimscript")
+  when hasFFI: defineSymbol("nimffi")
+  registerPass(verbosePass)
+  registerPass(semPass)
+  registerPass(evalPass)
+
+proc commandInteractive(graph: ModuleGraph; cache: IdentCache) =
+  msgs.gErrorMax = high(int)  # do not stop after first error
+  interactivePasses(graph, cache)
+  compileSystemModule(graph, cache)
+  if commandArgs.len > 0:
+    discard graph.compileModule(fileInfoIdx(gProjectFull), cache, {})
+  else:
+    var m = graph.makeStdinModule()
+    incl(m.flags, sfMainModule)
+    processModule(graph, m, llStreamOpenStdIn(), nil, cache)
+
+const evalPasses = [verbosePass, semPass, evalPass]
+
+proc evalNim(graph: ModuleGraph; nodes: PNode, module: PSym; cache: IdentCache) =
+  carryPasses(graph, nodes, module, cache, evalPasses)
+
+proc commandEval(graph: ModuleGraph; cache: IdentCache; exp: string) =
+  if systemModule == nil:
+    interactivePasses(graph, cache)
+    compileSystemModule(graph, cache)
+  let echoExp = "echo \"eval\\t\", " & "repr(" & exp & ")"
+  evalNim(graph, echoExp.parseString(cache), makeStdinModule(graph), cache)
+
+proc commandScan(cache: IdentCache) =
+  var f = addFileExt(mainCommandArg(), NimExt)
+  var stream = llStreamOpen(f, fmRead)
+  if stream != nil:
+    var
+      L: TLexer
+      tok: TToken
+    initToken(tok)
+    openLexer(L, f, stream, cache)
+    while true:
+      rawGetTok(L, tok)
+      printTok(tok)
+      if tok.tokType == tkEof: break
+    closeLexer(L)
+  else:
+    rawMessage(errCannotOpenFile, f)
+
+const
+  SimulateCaasMemReset = false
+  PrintRopeCacheStats = false
+
+proc mainCommand*(graph: ModuleGraph; cache: IdentCache) =
+  when SimulateCaasMemReset:
+    gGlobalOptions.incl(optCaasEnabled)
+
+  # In "nim serve" scenario, each command must reset the registered passes
+  clearPasses()
+  gLastCmdTime = epochTime()
+  searchPaths.add(options.libpath)
+  when false: # gProjectFull.len != 0:
+    # current path is always looked first for modules
+    prependStr(searchPaths, gProjectPath)
+  setId(100)
+  case command.normalize
+  of "c", "cc", "compile", "compiletoc":
+    # compile means compileToC currently
+    gCmd = cmdCompileToC
+    commandCompileToC(graph, cache)
+  of "cpp", "compiletocpp":
+    gCmd = cmdCompileToCpp
+    defineSymbol("cpp")
+    commandCompileToC(graph, cache)
+  of "objc", "compiletooc":
+    gCmd = cmdCompileToOC
+    defineSymbol("objc")
+    commandCompileToC(graph, cache)
+  of "run":
+    gCmd = cmdRun
+    when hasTinyCBackend:
+      extccomp.setCC("tcc")
+      commandCompileToC(graph, cache)
+    else:
+      rawMessage(errInvalidCommandX, command)
+  of "js", "compiletojs":
+    gCmd = cmdCompileToJS
+    commandCompileToJS(graph, cache)
+  of "php":
+    gCmd = cmdCompileToPHP
+    commandCompileToJS(graph, cache)
+  of "doc":
+    wantMainModule()
+    gCmd = cmdDoc
+    loadConfigs(DocConfig, cache)
+    commandDoc()
+  of "doc2":
+    gCmd = cmdDoc
+    loadConfigs(DocConfig, cache)
+    defineSymbol("nimdoc")
+    commandDoc2(graph, cache, false)
+  of "rst2html":
+    gCmd = cmdRst2html
+    loadConfigs(DocConfig, cache)
+    commandRst2Html()
+  of "rst2tex":
+    gCmd = cmdRst2tex
+    loadConfigs(DocTexConfig, cache)
+    commandRst2TeX()
+  of "jsondoc":
+    wantMainModule()
+    gCmd = cmdDoc
+    loadConfigs(DocConfig, cache)
+    wantMainModule()
+    defineSymbol("nimdoc")
+    commandJson()
+  of "jsondoc2":
+    gCmd = cmdDoc
+    loadConfigs(DocConfig, cache)
+    wantMainModule()
+    defineSymbol("nimdoc")
+    commandDoc2(graph, cache, true)
+  of "buildindex":
+    gCmd = cmdDoc
+    loadConfigs(DocConfig, cache)
+    commandBuildIndex()
+  of "gendepend":
+    gCmd = cmdGenDepend
+    commandGenDepend(graph, cache)
+  of "dump":
+    gCmd = cmdDump
+    if getConfigVar("dump.format") == "json":
+      wantMainModule()
+
+      var definedSymbols = newJArray()
+      for s in definedSymbolNames(): definedSymbols.elems.add(%s)
+
+      var libpaths = newJArray()
+      for dir in searchPaths: libpaths.elems.add(%dir)
+
+      var dumpdata = % [
+        (key: "version", val: %VersionAsString),
+        (key: "project_path", val: %gProjectFull),
+        (key: "defined_symbols", val: definedSymbols),
+        (key: "lib_paths", val: libpaths)
+      ]
+
+      msgWriteln($dumpdata, {msgStdout, msgSkipHook})
+    else:
+      msgWriteln("-- list of currently defined symbols --",
+                 {msgStdout, msgSkipHook})
+      for s in definedSymbolNames(): msgWriteln(s, {msgStdout, msgSkipHook})
+      msgWriteln("-- end of list --", {msgStdout, msgSkipHook})
+
+      for it in searchPaths: msgWriteln(it)
+  of "check":
+    gCmd = cmdCheck
+    commandCheck(graph, cache)
+  of "parse":
+    gCmd = cmdParse
+    wantMainModule()
+    discard parseFile(gProjectMainIdx, cache)
+  of "scan":
+    gCmd = cmdScan
+    wantMainModule()
+    commandScan(cache)
+    msgWriteln("Beware: Indentation tokens depend on the parser's state!")
+  of "secret":
+    gCmd = cmdInteractive
+    commandInteractive(graph, cache)
+  of "e":
+    commandEval(graph, cache, mainCommandArg())
+  of "nop", "help":
+    # prevent the "success" message:
+    gCmd = cmdDump
+  of "jsonscript":
+    gCmd = cmdJsonScript
+    commandJsonScript(graph, cache)
+  else:
+    rawMessage(errInvalidCommandX, command)
+
+  if msgs.gErrorCounter == 0 and
+     gCmd notin {cmdInterpret, cmdRun, cmdDump}:
+    when declared(system.getMaxMem):
+      let usedMem = formatSize(getMaxMem()) & " peakmem"
+    else:
+      let usedMem = formatSize(getTotalMem())
+    rawMessage(hintSuccessX, [$gLinesCompiled,
+               formatFloat(epochTime() - gLastCmdTime, ffDecimal, 3),
+               usedMem,
+               if condSyms.isDefined("release"): "Release Build"
+               else: "Debug Build"])
+
+  when PrintRopeCacheStats:
+    echo "rope cache stats: "
+    echo "  tries : ", gCacheTries
+    echo "  misses: ", gCacheMisses
+    echo "  int tries: ", gCacheIntTries
+    echo "  efficiency: ", formatFloat(1-(gCacheMisses.float/gCacheTries.float),
+                                       ffDecimal, 3)
+
+  when SimulateCaasMemReset:
+    resetMemory()
+
+  resetAttributes()
+
+proc mainCommand*() = mainCommand(newModuleGraph(newConfigRef()), newIdentCache())
---
+++ compiler/options.nim
@@ -0,0 +1,471 @@
+#
+#
+#           The Nim Compiler
+#        (c) Copyright 2015 Andreas Rumpf
+#
+#    See the file "copying.txt", included in this
+#    distribution, for details about the copyright.
+#
+
+import
+  os, strutils, strtabs, osproc, sets
+
+const
+  hasTinyCBackend* = defined(tinyc)
+  useEffectSystem* = true
+  useWriteTracking* = false
+  hasFFI* = defined(useFFI)
+  newScopeForIf* = true
+  useCaas* = not defined(noCaas)
+  noTimeMachine* = defined(avoidTimeMachine) and defined(macosx)
+
+type                          # please make sure we have under 32 options
+                              # (improves code efficiency a lot!)
+  TOption* = enum             # **keep binary compatible**
+    optNone, optObjCheck, optFieldCheck, optRangeCheck, optBoundsCheck,
+    optOverflowCheck, optNilCheck,
+    optNaNCheck, optInfCheck,
+    optAssert, optLineDir, optWarns, optHints,
+    optOptimizeSpeed, optOptimizeSize, optStackTrace, # stack tracing support
+    optLineTrace,             # line tracing support (includes stack tracing)
+    optEndb,                  # embedded debugger
+    optByRef,                 # use pass by ref for objects
+                              # (for interfacing with C)
+    optProfiler,              # profiler turned on
+    optImplicitStatic,        # optimization: implicit at compile time
+                              # evaluation
+    optPatterns,              # en/disable pattern matching
+    optMemTracker
+
+  TOptions* = set[TOption]
+  TGlobalOption* = enum       # **keep binary compatible**
+    gloptNone, optForceFullMake, optDeadCodeElim,
+    optListCmd, optCompileOnly, optNoLinking,
+    optCDebug,                # turn on debugging information
+    optGenDynLib,             # generate a dynamic library
+    optGenStaticLib,          # generate a static library
+    optGenGuiApp,             # generate a GUI application
+    optGenScript,             # generate a script file to compile the *.c files
+    optGenMapping,            # generate a mapping file
+    optRun,                   # run the compiled project
+    optSymbolFiles,           # use symbol files for speeding up compilation
+    optCaasEnabled            # compiler-as-a-service is running
+    optSkipConfigFile,        # skip the general config file
+    optSkipProjConfigFile,    # skip the project's config file
+    optSkipUserConfigFile,    # skip the users's config file
+    optSkipParentConfigFiles, # skip parent dir's config files
+    optNoMain,                # do not generate a "main" proc
+    optUseColors,             # use colors for hints, warnings, and errors
+    optThreads,               # support for multi-threading
+    optStdout,                # output to stdout
+    optThreadAnalysis,        # thread analysis pass
+    optTaintMode,             # taint mode turned on
+    optTlsEmulation,          # thread var emulation turned on
+    optGenIndex               # generate index file for documentation;
+    optEmbedOrigSrc           # embed the original source in the generated code
+                              # also: generate header file
+    optIdeDebug               # idetools: debug mode
+    optIdeTerse               # idetools: use terse descriptions
+    optNoCppExceptions        # use C exception handling even with CPP
+    optExcessiveStackTrace    # fully qualified module filenames
+
+  TGlobalOptions* = set[TGlobalOption]
+
+const
+  harmlessOptions* = {optForceFullMake, optNoLinking, optRun,
+                      optUseColors, optStdout}
+
+type
+  TCommands* = enum           # Nim's commands
+                              # **keep binary compatible**
+    cmdNone, cmdCompileToC, cmdCompileToCpp, cmdCompileToOC,
+    cmdCompileToJS,
+    cmdCompileToPHP,
+    cmdCompileToLLVM, cmdInterpret, cmdPretty, cmdDoc,
+    cmdGenDepend, cmdDump,
+    cmdCheck,                 # semantic checking for whole project
+    cmdParse,                 # parse a single file (for debugging)
+    cmdScan,                  # scan a single file (for debugging)
+    cmdIdeTools,              # ide tools
+    cmdDef,                   # def feature (find definition for IDEs)
+    cmdRst2html,              # convert a reStructuredText file to HTML
+    cmdRst2tex,               # convert a reStructuredText file to TeX
+    cmdInteractive,           # start interactive session
+    cmdRun,                   # run the project via TCC backend
+    cmdJsonScript             # compile a .json build file
+  TStringSeq* = seq[string]
+  TGCMode* = enum             # the selected GC
+    gcNone, gcBoehm, gcGo, gcRegions, gcMarkAndSweep, gcRefc,
+    gcV2, gcGenerational
+
+  IdeCmd* = enum
+    ideNone, ideSug, ideCon, ideDef, ideUse, ideDus, ideChk, ideMod,
+    ideHighlight, ideOutline, ideKnown, ideMsg
+
+  ConfigRef* = ref object ## eventually all global configuration should be moved here
+    cppDefines*: HashSet[string]
+    headerFile*: string
+
+proc newConfigRef*(): ConfigRef =
+  result = ConfigRef(cppDefines: initSet[string](),
+    headerFile: "")
+
+proc cppDefine*(c: ConfigRef; define: string) =
+  c.cppDefines.incl define
+
+var
+  gIdeCmd*: IdeCmd
+
+const
+  ChecksOptions* = {optObjCheck, optFieldCheck, optRangeCheck, optNilCheck,
+    optOverflowCheck, optBoundsCheck, optAssert, optNaNCheck, optInfCheck}
+
+var
+  gOptions*: TOptions = {optObjCheck, optFieldCheck, optRangeCheck,
+                         optBoundsCheck, optOverflowCheck, optAssert, optWarns,
+                         optHints, optStackTrace, optLineTrace,
+                         optPatterns, optNilCheck}
+  gGlobalOptions*: TGlobalOptions = {optThreadAnalysis}
+  gExitcode*: int8
+  gCmd*: TCommands = cmdNone  # the command
+  gSelectedGC* = gcRefc       # the selected GC
+  searchPaths*: seq[string] = @[]
+  lazyPaths*: seq[string]   = @[]
+  outFile*: string = ""
+  docSeeSrcUrl*: string = ""  # if empty, no seeSrc will be generated. \
+  # The string uses the formatting variables `path` and `line`.
+  #headerFile*: string = ""
+  gVerbosity* = 1             # how verbose the compiler is
+  gNumberOfProcessors*: int   # number of processors
+  gWholeProject*: bool        # for 'doc2': output any dependency
+  gEvalExpr* = ""             # expression for idetools --eval
+  gLastCmdTime*: float        # when caas is enabled, we measure each command
+  gListFullPaths*: bool
+  isServing*: bool = false
+  gNoNimblePath* = false
+  gExperimentalMode*: bool
+
+proc importantComments*(): bool {.inline.} = gCmd in {cmdDoc, cmdIdeTools}
+proc usesNativeGC*(): bool {.inline.} = gSelectedGC >= gcRefc
+
+template compilationCachePresent*: untyped =
+  {optCaasEnabled, optSymbolFiles} * gGlobalOptions != {}
+
+template optPreserveOrigSource*: untyped =
+  optEmbedOrigSrc in gGlobalOptions
+
+const
+  genSubDir* = "nimcache"
+  NimExt* = "nim"
+  RodExt* = "rod"
+  HtmlExt* = "html"
+  JsonExt* = "json"
+  TexExt* = "tex"
+  IniExt* = "ini"
+  DefaultConfig* = "nim.cfg"
+  DocConfig* = "nimdoc.cfg"
+  DocTexConfig* = "nimdoc.tex.cfg"
+
+# additional configuration variables:
+var
+  gConfigVars* = newStringTable(modeStyleInsensitive)
+  gDllOverrides = newStringTable(modeCaseInsensitive)
+  gModuleOverrides* = newStringTable(modeStyleInsensitive)
+  gPrefixDir* = "" # Overrides the default prefix dir in getPrefixDir proc.
+  libpath* = ""
+  gProjectName* = "" # holds a name like 'nim'
+  gProjectPath* = "" # holds a path like /home/alice/projects/nim/compiler/
+  gProjectFull* = "" # projectPath/projectName
+  gProjectIsStdin* = false # whether we're compiling from stdin
+  gProjectMainIdx*: int32 # the canonical path id of the main module
+  nimcacheDir* = ""
+  command* = "" # the main command (e.g. cc, check, scan, etc)
+  commandArgs*: seq[string] = @[] # any arguments after the main command
+  gKeepComments*: bool = true # whether the parser needs to keep comments
+  implicitImports*: seq[string] = @[] # modules that are to be implicitly imported
+  implicitIncludes*: seq[string] = @[] # modules that are to be implicitly included
+
+const oKeepVariableNames* = true
+
+template compilingLib*: bool =
+  gGlobalOptions * {optGenGuiApp, optGenDynLib} != {}
+
+proc mainCommandArg*: string =
+  ## This is intended for commands like check or parse
+  ## which will work on the main project file unless
+  ## explicitly given a specific file argument
+  if commandArgs.len > 0:
+    result = commandArgs[0]
+  else:
+    result = gProjectName
+
+proc existsConfigVar*(key: string): bool =
+  result = hasKey(gConfigVars, key)
+
+proc getConfigVar*(key: string): string =
+  result = gConfigVars.getOrDefault key
+
+proc setConfigVar*(key, val: string) =
+  gConfigVars[key] = val
+
+proc getOutFile*(filename, ext: string): string =
+  if options.outFile != "": result = options.outFile
+  else: result = changeFileExt(filename, ext)
+
+proc getPrefixDir*(): string =
+  ## Gets the prefix dir, usually the parent directory where the binary resides.
+  ##
+  ## This is overridden by some tools (namely nimsuggest) via the ``gPrefixDir``
+  ## global.
+  if gPrefixDir != "": result = gPrefixDir
+  else:
+    result = splitPath(getAppDir()).head
+
+proc setDefaultLibpath*() =
+  # set default value (can be overwritten):
+  if libpath == "":
+    # choose default libpath:
+    var prefix = getPrefixDir()
+    when defined(posix):
+      if prefix == "/usr": libpath = "/usr/lib/nim"
+      elif prefix == "/usr/local": libpath = "/usr/local/lib/nim"
+      else: libpath = joinPath(prefix, "lib")
+    else: libpath = joinPath(prefix, "lib")
+
+    # Special rule to support other tools (nimble) which import the compiler
+    # modules and make use of them.
+    let realNimPath = findExe("nim")
+    # Find out if $nim/../../lib/system.nim exists.
+    let parentNimLibPath = realNimPath.parentDir().parentDir() / "lib"
+    if not fileExists(libpath / "system.nim") and
+        fileExists(parentNimlibPath / "system.nim"):
+      libpath = parentNimLibPath
+
+proc canonicalizePath*(path: string): string =
+  # on Windows, 'expandFilename' calls getFullPathName which doesn't do
+  # case corrections, so we have to use this convoluted way of retrieving
+  # the true filename (see tests/modules and Nimble uses 'import Uri' instead
+  # of 'import uri'):
+  when defined(windows):
+    result = path.expandFilename
+    for x in walkFiles(result):
+      return x
+  else:
+    result = path.expandFilename
+
+proc shortenDir*(dir: string): string =
+  ## returns the interesting part of a dir
+  var prefix = gProjectPath & DirSep
+  if startsWith(dir, prefix):
+    return substr(dir, len(prefix))
+  prefix = getPrefixDir() & DirSep
+  if startsWith(dir, prefix):
+    return substr(dir, len(prefix))
+  result = dir
+
+proc removeTrailingDirSep*(path: string): string =
+  if (len(path) > 0) and (path[len(path) - 1] == DirSep):
+    result = substr(path, 0, len(path) - 2)
+  else:
+    result = path
+
+proc disableNimblePath*() =
+  gNoNimblePath = true
+  lazyPaths.setLen(0)
+
+include packagehandling
+
+proc getNimcacheDir*: string =
+  result = if nimcacheDir.len > 0: nimcacheDir else: gProjectPath.shortenDir /
+                                                         genSubDir
+
+
+proc pathSubs*(p, config: string): string =
+  let home = removeTrailingDirSep(os.getHomeDir())
+  result = unixToNativePath(p % [
+    "nim", getPrefixDir(),
+    "lib", libpath,
+    "home", home,
+    "config", config,
+    "projectname", options.gProjectName,
+    "projectpath", options.gProjectPath,
+    "projectdir", options.gProjectPath,
+    "nimcache", getNimcacheDir()])
+  if "~/" in result:
+    result = result.replace("~/", home & '/')
+
+proc toGeneratedFile*(path, ext: string): string =
+  ## converts "/home/a/mymodule.nim", "rod" to "/home/a/nimcache/mymodule.rod"
+  var (head, tail) = splitPath(path)
+  #if len(head) > 0: head = shortenDir(head & dirSep)
+  result = joinPath([getNimcacheDir(), changeFileExt(tail, ext)])
+  #echo "toGeneratedFile(", path, ", ", ext, ") = ", result
+
+when noTimeMachine:
+  var alreadyExcludedDirs = initSet[string]()
+  proc excludeDirFromTimeMachine(dir: string) {.raises: [].} =
+    ## Calls a macosx command on the directory to exclude it from backups.
+    ##
+    ## The macosx tmutil command is invoked to mark the specified path as an
+    ## item to be excluded from time machine backups. If a path already exists
+    ## with files before excluding it, newer files won't be added to the
+    ## directory, but previous files won't be removed from the backup until the
+    ## user deletes that directory.
+    ##
+    ## The whole proc is optional and will ignore all kinds of errors. The only
+    ## way to be sure that it works is to call ``tmutil isexcluded path``.
+    if alreadyExcludedDirs.contains(dir): return
+    alreadyExcludedDirs.incl(dir)
+    try:
+      var p = startProcess("/usr/bin/tmutil", args = ["addexclusion", dir])
+      discard p.waitForExit
+      p.close
+    except Exception:
+      discard
+
+proc completeGeneratedFilePath*(f: string, createSubDir: bool = true): string =
+  var (head, tail) = splitPath(f)
+  #if len(head) > 0: head = removeTrailingDirSep(shortenDir(head & dirSep))
+  var subdir = getNimcacheDir() # / head
+  if createSubDir:
+    try:
+      createDir(subdir)
+      when noTimeMachine:
+        excludeDirFromTimeMachine(subdir)
+    except OSError:
+      writeLine(stdout, "cannot create directory: " & subdir)
+      quit(1)
+  result = joinPath(subdir, tail)
+  #echo "completeGeneratedFilePath(", f, ") = ", result
+
+proc rawFindFile(f: string): string =
+  for it in searchPaths:
+    result = joinPath(it, f)
+    if existsFile(result):
+      return result.canonicalizePath
+  result = ""
+
+proc rawFindFile2(f: string): string =
+  for i, it in lazyPaths:
+    result = joinPath(it, f)
+    if existsFile(result):
+      # bring to front
+      for j in countDown(i,1):
+        swap(lazyPaths[j], lazyPaths[j-1])
+
+      return result.canonicalizePath
+  result = ""
+
+template patchModule() {.dirty.} =
+  if result.len > 0 and gModuleOverrides.len > 0:
+    let key = getPackageName(result) & "_" & splitFile(result).name
+    if gModuleOverrides.hasKey(key):
+      let ov = gModuleOverrides[key]
+      if ov.len > 0: result = ov
+
+proc findFile*(f: string): string {.procvar.} =
+  if f.isAbsolute:
+    result = if f.existsFile: f else: ""
+  else:
+    result = f.rawFindFile
+    if result.len == 0:
+      result = f.toLowerAscii.rawFindFile
+      if result.len == 0:
+        result = f.rawFindFile2
+        if result.len == 0:
+          result = f.toLowerAscii.rawFindFile2
+  patchModule()
+
+proc findModule*(modulename, currentModule: string): string =
+  # returns path to module
+  when defined(nimfix):
+    # '.nimfix' modules are preferred over '.nim' modules so that specialized
+    # versions can be kept for 'nimfix'.
+    block:
+      let m = addFileExt(modulename, "nimfix")
+      let currentPath = currentModule.splitFile.dir
+      result = currentPath / m
+      if not existsFile(result):
+        result = findFile(m)
+        if existsFile(result): return result
+  let m = addFileExt(modulename, NimExt)
+  let currentPath = currentModule.splitFile.dir
+  result = currentPath / m
+  if not existsFile(result):
+    result = findFile(m)
+  patchModule()
+
+proc findProjectNimFile*(pkg: string): string =
+  const extensions = [".nims", ".cfg", ".nimcfg", ".nimble"]
+  var candidates: seq[string] = @[]
+  for k, f in os.walkDir(pkg, relative=true):
+    if k == pcFile and f != "config.nims":
+      let (_, name, ext) = splitFile(f)
+      if ext in extensions:
+        let x = changeFileExt(pkg / name, ".nim")
+        if fileExists(x):
+          candidates.add x
+  for c in candidates:
+    # nim-foo foo  or  foo  nfoo
+    if (pkg in c) or (c in pkg): return c
+  if candidates.len >= 1:
+    return candidates[0]
+  return ""
+
+proc canonDynlibName(s: string): string =
+  let start = if s.startsWith("lib"): 3 else: 0
+  let ende = strutils.find(s, {'(', ')', '.'})
+  if ende >= 0:
+    result = s.substr(start, ende-1)
+  else:
+    result = s.substr(start)
+
+proc inclDynlibOverride*(lib: string) =
+  gDllOverrides[lib.canonDynlibName] = "true"
+
+proc isDynlibOverride*(lib: string): bool =
+  result = gDllOverrides.hasKey(lib.canonDynlibName)
+
+proc binaryStrSearch*(x: openArray[string], y: string): int =
+  var a = 0
+  var b = len(x) - 1
+  while a <= b:
+    var mid = (a + b) div 2
+    var c = cmpIgnoreCase(x[mid], y)
+    if c < 0:
+      a = mid + 1
+    elif c > 0:
+      b = mid - 1
+    else:
+      return mid
+  result = - 1
+
+proc parseIdeCmd*(s: string): IdeCmd =
+  case s:
+  of "sug": ideSug
+  of "con": ideCon
+  of "def": ideDef
+  of "use": ideUse
+  of "dus": ideDus
+  of "chk": ideChk
+  of "mod": ideMod
+  of "highlight": ideHighlight
+  of "outline": ideOutline
+  of "known": ideKnown
+  of "msg": ideMsg
+  else: ideNone
+
+proc `$`*(c: IdeCmd): string =
+  case c:
+  of ideSug: "sug"
+  of ideCon: "con"
+  of ideDef: "def"
+  of ideUse: "use"
+  of ideDus: "dus"
+  of ideChk: "chk"
+  of ideMod: "mod"
+  of ideNone: "none"
+  of ideHighlight: "highlight"
+  of ideOutline: "outline"
+  of ideKnown: "known"
+  of ideMsg: "msg"
