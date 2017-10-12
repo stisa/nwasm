bdc10414a0550549e886165dc8c6c8bc00b3e871
stisa <stisa4@gmail.com>
2017-10-12 22:50:22 +0200

    update progress

--- README.md
+++ README.md
@@ -2,4 +2,4 @@ NWAsm
 =====
 
 Experimenting with a webassembly backend for nim.  
-Currently tryign to get `system.nim` to compile, roughly 200 lines out of 4000 work.
+Currently tryign to get `system.nim` to compile, roughly 650 lines out of 4000 work.
--- test/test.nim
+++ test/test.nim
@@ -1,18 +1,50 @@
 #proc log(x:string) {.header:"glue", importc:"rawEcho".}
 proc log* [T](x:T) {.used, header:"glue", importc:"log".}
 
-type A = ref object
-  id: int
-  f: float32
-var 
-  b : A
-new b
-log(b.f) # == 0)
-b.id = 13
 
-log(b.id) # == 13)
-b.f = 3.14'f32
-log(b.f) # == 3.14)
-reset b
-log(b.id) # == 0)
-log(b.f) # == 0.0)
+type A = enum
+  aa, ab, ac
+
+var 
+  a = ab
+  b = aa
+log(a <= b)
+log(a <= ab)
+
+var
+  pa = addr a
+  pb = addr b
+log(pa <= pb)
+
+var
+  sa = "hello"
+  sb = "world"
+log(sa <= sb)
+
+var
+  ca = 'x'
+  cb = 'y'
+log(ca <= cb)
+
+var
+  ba = true
+  bb = false
+log(ba <= bb)
+
+#[
+var 
+ta = {aa,ab}
+tb = {ac}
+log(ta == tb)
+]#
+
+var
+  ra : ref int
+  rb : ref int
+new ra
+new rb
+log(ra <= rb)
+ra = rb
+log(ra <= rb)
+
+#[Mising: EqProc]#
